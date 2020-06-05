2b67a00cd1189bf6f09905f77f26eec375ab5794
Colin <colintklein@gmail.com>
2020-06-05 10:13:25 -0500

    Updated controllers for new attributes, added gender and age attributes to actors model, created partial for movie view

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -14,7 +14,9 @@ class Api::MoviesController < ApplicationController
     @movie = Movie.create(
       title: params[:title],
       year: params[:year],
-      plot: params[:plot]
+      plot: params[:plot],
+      director: params[:director],
+      english: params[:english]
     )
     render "show.json.jb"
   end
@@ -24,7 +26,7 @@ class Api::MoviesController < ApplicationController
     @movie.title = params[:title] || @movie.title
     @movie.year = params[:year] || @movie.year
     @movie.plot = params[:plot] || @movie.plot
-    @movie.plot = params[:plot] || @movie.plot
+    @movie.director = params[:director] || @movie.director
     @movie.english = params[:english] || @movie.english
     @movie.save
     render "show.json.jb"
