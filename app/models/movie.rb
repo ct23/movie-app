7c97b46676317e242d51c13ccb6db18b3036ac29
Colin <colintklein@gmail.com>
2020-06-08 10:25:16 -0500

    Adds validations for movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ApplicationRecord
+  validates :title, :year, presence: true
+  validates :year, numericality: { greater_than: 1500, less_than: 2100 }
+  validates :director, :title, length: { minimum: 2 }
 end
