8c54062978c02761ccc4bacf3bbb08526941bc0b
Colin <colintklein@gmail.com>
2020-06-21 16:44:44 -0500

    Adds Genre model and MovieGenres association model. Adds proper association methods and some data. Adds genre_names method to Movie model to help with JSON output.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,13 @@ class Movie < ApplicationRecord
   validates :director, :title, length: { minimum: 2 }
 
   has_many :actors
+
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
+
+  def genre_names
+    genres.map do |genre|
+      genre.name
+    end
+  end
 end
